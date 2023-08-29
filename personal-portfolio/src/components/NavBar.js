0d12f5e6695c20c2172517c6c531d697d28a85f4
lucianatux <lucianatux@yahoo.com.ar>
2023-08-29 18:09:58 -0300

    estilos

--- personal-portfolio/src/components/NavBar.js
+++ personal-portfolio/src/components/NavBar.js
@@ -3,6 +3,11 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { useState, useEffect } from 'react';
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
+
 
 export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home');
@@ -30,7 +35,7 @@ export const NavBar = () => {
     <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
       <Container>
         <Navbar.Brand href="#home">
-            <img src={''} alt="logo" />
+            <img src={logo} alt="logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
@@ -63,9 +68,9 @@ export const NavBar = () => {
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-                <a href="#"><img src={''} alt="" /></a>
-                <a href="#"><img src={''} alt="" /></a>
-                <a href="#"><img src={''} alt="" /></a>
+                <a href="#"><img src={navIcon1} alt="" /></a>
+                <a href="#"><img src={navIcon2} alt="" /></a>
+                <a href="#"><img src={navIcon3} alt="" /></a>
             </div>
             <button className='vvd' onClick={()=>console.log('connect')}>
                 <span>Let´s connect</span>
