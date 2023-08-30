d7209d0a4d871231ba2e6dac76892edbeb09ae70
lucianatux <lucianatux@yahoo.com.ar>
2023-08-30 19:20:21 -0300

    contact

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -4,6 +4,8 @@ import {NavBar} from "./components/NavBar";
 import {Banner} from "./components/Banner";
 import {Skills} from "./components/Skills";
 import {Projects} from "./components/Projects";
+import {Contact} from "./components/Contact";
+
 
 
 
@@ -16,6 +18,7 @@ function App() {
       <Banner/>
       <Skills/>
       <Projects/>
+      <Contact/>
     </div>
   );
 }
