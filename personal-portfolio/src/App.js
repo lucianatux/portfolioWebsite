af0847a97758b1bbf55ea27e88b43feafdaf47fe
lucianatux <lucianatux@yahoo.com.ar>
2023-08-30 16:02:51 -0300

    skills

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {NavBar} from "./components/NavBar";
 import {Banner} from "./components/Banner";
+import {Skills} from "./components/Skills";
+
 
 
 
@@ -10,6 +12,7 @@ function App() {
     <div className="App">
       <NavBar />
       <Banner/>
+      <Skills/>
     </div>
   );
 }
