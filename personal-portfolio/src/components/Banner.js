a212ad7bf3ce86a2ec3f6dd667a38f6d84f470b6
lucianatux <lucianatux@yahoo.com.ar>
2023-09-09 21:35:10 -0300

    universo

--- personal-portfolio/src/components/Banner.js
+++ personal-portfolio/src/components/Banner.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import headerImg from "../assets/img/header-img.png";
+import headerImg from "../assets/img/tierra2.png";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 
 export const Banner = () => {
